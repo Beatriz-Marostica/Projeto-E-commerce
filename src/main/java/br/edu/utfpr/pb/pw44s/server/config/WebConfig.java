31e945becf3ce4c5b8c20d76ec8041f1a34e0d97
Beatriz Marostica <mbea855@gmail.com>
2025-09-29 07:15:39 -0300

    primeira entrega do projeto

--- src/main/java/br/edu/utfpr/pb/pw44s/server/config/WebConfig.java
+++ src/main/java/br/edu/utfpr/pb/pw44s/server/config/WebConfig.java
@@ -1,28 +1,16 @@
 package br.edu.utfpr.pb.pw44s.server.config;
-
-import br.edu.utfpr.pb.pw44s.server.dto.EnderecoDTO;
-import br.edu.utfpr.pb.pw44s.server.model.Endereco;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.PropertyMap;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
-@Configuration
+@Component
 public class WebConfig {
 
     @Bean
     public ModelMapper modelMapper() {
-        ModelMapper modelMapper = new ModelMapper();
-
-        modelMapper.addMappings(new PropertyMap<EnderecoDTO, Endereco>() {
-            @Override
-            protected void configure() {
-                map(source.getUser().getId(), destination.getUser().getId());
-            }
-        });
-
-        return modelMapper;
+        return new ModelMapper();
     }
+
 }
 
 
